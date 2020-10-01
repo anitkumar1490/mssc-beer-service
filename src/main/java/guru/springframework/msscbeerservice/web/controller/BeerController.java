9ce7790cb480dac00a1c543d9fe62ac319b4721c
anit <anit.kumar@outlook.com>
2020-10-01 23:54:17 +0530

    Adding source to Git

---
+++ src/main/java/guru/springframework/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,35 @@
+package guru.springframework.msscbeerservice.web.controller;
+
+import guru.springframework.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId) {
+
+        //todo Impl
+       return new ResponseEntity<>(BeerDto.builder().build(), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity saveNewBeer(@Validated @RequestBody BeerDto beerDto) {
+
+        //todo Impl
+        return new ResponseEntity(HttpStatus.CREATED);
+    }
+
+    @PutMapping
+    public ResponseEntity updateBeerById(@PathVariable UUID beerId, @Validated @RequestBody BeerDto beerDto){
+
+        //todo Impl
+        return new ResponseEntity(HttpStatus.NO_CONTENT);
+    }
+}
